79c46945a6a04fa772edc5cace3474235a18359a
simon <lashwang@outlook.com>
2018-01-25 11:13:08 +0800

    add crcs analyser tool.

---
+++ crcs_analyser.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import fire
+
+
+
+
+
+
+
+class CRCSAnaLyser(object):
+    def file(self,f):
+        pass
+
+    def folder(self,d):
+        pass
+
+
+
+def main():
+    fire.Fire(CRCSAnaLyser)
+
+
+
+
+if __name__ == "__main__":
+    main()

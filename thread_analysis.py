c9ec8e8c9c3860e9747fbc105f71df3fed38851a
simon <lashwang@outlook.com>
2018-11-01 13:43:07 +0800

    add logcat parser tool

--- thread_analysis.py
+++ thread_analysis.py
@@ -5,21 +5,51 @@ import re
 from openpyxl import Workbook
 from openpyxl.compat import range
 from openpyxl.utils import get_column_letter
+from abc import ABCMeta, abstractmethod
+
+
 
 
 LOGCAT_DEFAULT_PATH = "/Users/simon/work/svn/dev_oc/engineering/projects/oc_client/dev/adclear_4_0/logcat.log"
 
-def parse_file(fname):
+
+class LogParserBase():
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        self.is_java = False
+
+
+
+    @staticmethod
+    def parse_line(line):
+        log_parser = LogParserBase()
+
+
+        if "[Native]" in line:
+            log_parser.is_java = True
+        elif "[JAVA]" in line:
+            log_parser.is_java = False
+        else:
+            return None
+
+
+        return log_parser
+
+
+def parse_file(fname=None):
 
     if not fname:
         fname = LOGCAT_DEFAULT_PATH
 
-    dest_filename = fname + ".xlsx";
+    output_filename = fname + ".xlsx";
 
     with open(fname) as f:
         content = f.readlines()
 
     for line in content:
+        log_parser = LogParserBase.parse_line(line)
+        print log_parser
         pass
 
 

42a07816f051bf18ffb2a56d84a991d3759c0dc1
simon <lashwang@outlook.com>
2018-11-01 15:12:39 +0800

    add logcat parser tool

--- thread_analysis.py
+++ thread_analysis.py
@@ -12,12 +12,62 @@ from abc import ABCMeta, abstractmethod
 
 LOGCAT_DEFAULT_PATH = "/Users/simon/work/svn/dev_oc/engineering/projects/oc_client/dev/adclear_4_0/logcat.log"
 
+log_all_list = []
+log_thread_dic = {}
+
+
 
 class LogParserBase():
     __metaclass__ = ABCMeta
+    parer_function = None
+
 
     def __init__(self):
         self.is_java = False
+        self.orig_line = None
+        self.csm = None
+
+
+
+    @staticmethod
+    def parse_format_1(self):
+        reg_str = r"^\S+\((\S+)\):\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+\[\S+\]\s+(.*)"
+        matchObj = re.search(reg_str, self.orig_line)
+        if matchObj:
+            self.pid = matchObj.group(1)
+            self.tid = matchObj.group(5)
+        else:
+            return False
+
+        if not self.pid.isdigit():
+            return False
+
+        if not self.tid.isdigit():
+            return False
+
+        self.date = matchObj.group(2)
+        self.time = matchObj.group(3)
+        self.log = matchObj.group(6)
+
+        if not self.is_java:
+            reg_str = "^\[(\S+):(\d+)\]\s+\((\S+)\)\s+\-\s+(.*)"
+            matchObj = re.search(reg_str, self.log)
+            if matchObj:
+                self.filename = matchObj.group(1)
+                self.fileline = matchObj.group(2)
+                self.log = matchObj.group(4)
+            else:
+                return False
+
+        reg_str = r"(.*)\[([0-9a-fA-F]{8})\](.*)"
+        matchObj = re.search(reg_str, self.log)
+        if matchObj:
+            self.csm = matchObj.group(2)
+        else:
+            self.csm = None
+
+        return True
+
 
 
 
@@ -27,11 +77,23 @@ class LogParserBase():
 
 
         if "[Native]" in line:
-            log_parser.is_java = True
-        elif "[JAVA]" in line:
             log_parser.is_java = False
+        elif "[JAVA]" in line:
+            log_parser.is_java = True
         else:
             return None
+
+        log_parser.orig_line = line
+
+
+        if not LogParserBase.parer_function:
+            if LogParserBase.parse_format_1(log_parser):
+                LogParserBase.parer_function = LogParserBase.parse_format_1
+        else:
+            ret = LogParserBase.parer_function(log_parser)
+            if not ret:
+                #print "Unkown line:" + line
+                return None
 
 
         return log_parser
@@ -49,8 +111,14 @@ def parse_file(fname=None):
 
     for line in content:
         log_parser = LogParserBase.parse_line(line)
-        print log_parser
-        pass
+        if not log_parser:
+            continue
+        log_all_list.append(log_parser)
+        key = log_parser.pid + '-' + log_parser.tid
+        log_thread_dic[key] = log_parser
+
+
+    print "process finished."
 
 
 def main():

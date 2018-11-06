2a836fb23ff1d986a122df35b4e0f40de1294273
simon <lashwang@outlook.com>
2018-11-06 14:44:13 +0800

    add logcat parser tool

--- thread_analysis.py
+++ thread_analysis.py
@@ -13,11 +13,12 @@ import arrow
 LOGCAT_DEFAULT_PATH = "/Users/simon/Downloads/Logcat_d5c50fd4106d8d14_2018-10-31-00.00.00_2018-11-01-00.00.00-r-00000"
 
 log_all_list = []
-log_thread_dic = {}
 
+wb = Workbook()
+ws_all = wb.active
+ws_all.title = "all"
 
-
-class LogParserBase():
+class Parser():
     __metaclass__ = ABCMeta
     parer_function = None
     last_thread_record = {}
@@ -131,7 +132,7 @@ class LogParserBase():
 
     @staticmethod
     def parse_line(line):
-        log_parser = LogParserBase()
+        log_parser = Parser()
 
 
         if "[Native]" in line:
@@ -144,13 +145,13 @@ class LogParserBase():
         log_parser.orig_line = line
 
 
-        if not LogParserBase.parer_function:
-            if LogParserBase.parse_format_1(log_parser):
-                LogParserBase.parer_function = LogParserBase.parse_format_1
-            elif LogParserBase.parse_format_2(log_parser):
-                LogParserBase.parer_function = LogParserBase.parse_format_2
+        if not Parser.parer_function:
+            if Parser.parse_format_1(log_parser):
+                Parser.parer_function = Parser.parse_format_1
+            elif Parser.parse_format_2(log_parser):
+                Parser.parer_function = Parser.parse_format_2
         else:
-            ret = LogParserBase.parer_function(log_parser)
+            ret = Parser.parer_function(log_parser)
             if not ret:
                 #print "Unkown line:" + line
                 return None
@@ -158,6 +159,18 @@ class LogParserBase():
 
         return log_parser
 
+    def to_ws_string(self):
+        return [self.date,
+                self.time,
+                self.pid,
+                self.tid,
+                self.csm,
+                self.thread_time_delay,
+                str(self.filename) + ":" + str(self.fileline),
+                self.log]
+    @staticmethod
+    def to_ws_string_header():
+        return "date,time,pid,tid,csm,thread_delay,fileline,log".split(",")
 
 def calc_time_diff(line1,line2):
     year = arrow.now().datetime.year
@@ -169,10 +182,11 @@ def calc_time_diff(line1,line2):
     date_str = date_str + " " + line2.time
     t2 = arrow.get(date_str, "YYYY-MM-DD HH:mm:ss.SSS")
 
-    time_diff = (t1-t2).microseconds/1000
+    time_delta = (t1-t2)
 
-    if time_diff > 100:
-        pass
+
+    time_diff = time_delta.total_seconds()*1000
+
 
     return time_diff
 
@@ -182,23 +196,29 @@ def parse_file(fname=None):
     if not fname:
         fname = LOGCAT_DEFAULT_PATH
 
-    output_filename = fname + ".xlsx";
+    output_filename = fname + ".xlsx"
 
     with open(fname) as f:
         content = f.readlines()
 
+    ws_all.append(Parser.to_ws_string_header())
     for line in content:
-        log_parser = LogParserBase.parse_line(line)
+        log_parser = Parser.parse_line(line)
         if not log_parser:
             continue
         log_all_list.append(log_parser)
         key = log_parser.pid + "-" + log_parser.tid
-        if(LogParserBase.last_thread_record.has_key(key)):
-            log_parser.thread_time_delay = calc_time_diff(log_parser, LogParserBase.last_thread_record[key])
+        if(Parser.last_thread_record.has_key(key)):
+            log_parser.thread_time_delay = calc_time_diff(log_parser, Parser.last_thread_record[key])
         else:
             log_parser.thread_time_delay = 0
 
-        LogParserBase.last_thread_record[key] = log_parser
+        Parser.last_thread_record[key] = log_parser
+
+        ws_all.append(log_parser.to_ws_string())
+
+    wb.save(filename=output_filename)
+
 
     print "process finished."
 

fed91a2637e6e81c3f0c79725ea43520c01e88e3
simon <lashwang@outlook.com>
2019-01-02 16:45:33 +0800

    add logcat query tools

--- logcat_query.py
+++ logcat_query.py
@@ -46,6 +46,45 @@ def find_host(line,saved_tables):
         saved_tables[csm_id] = host
 
 
+def find_elem_hide_url(line,saved_tables):
+    '''
+    01-02 15:36:18.883 11958 12306 V [Native]proxy: 01-02 15:36:18.883 +0800 12306 [T]
+    [http_task.cpp:646] (0) -
+    CSM [000C806E] HTRX [000C030E] Enable element hide for url
+    https://m.youtube.com/csi_204?GetBrowse_rid=0x38f07bc418e6f1f7&action=home&csn=8mksXPiPOIrhigT5u5XQBg&c=MWEB&cver=2.20181220&h2pc=0&is_nav=1&pis=d&p=HTTP/1.1&rc=mcj&rt=ai.2962,apr.3001,srt.1805,fpt.3068,nreqs.1209,nress.1805,nrese.2680,ntcps.22,ntcpe.1208,nstcps.348,rsf_mcj.1873,rse_mcj.1873,pdr.3035,ips.3060,ipc.3106,cpt.3107,vpl.3226,aft.3107,ps.3310&s=youtube_mobile&st=717&t=tcp&v=2&vpil=2&vpni=2&yt_fn=what_to_watch&yt_li=1&yt_lt=cold&yt_sts=n&yt_vis=1
+    :return:
+    '''
+    if not "Enable element hide for url" in line:
+        return
+
+    reg_str = r"CSM\s+\[(\S+)\].+Enable element hide for url (.*)"
+
+    matchObj = re.search(reg_str, line)
+    if matchObj:
+        csm_id = matchObj.group(1)
+        url = matchObj.group(2)
+        saved_tables[csm_id] = url
+
+
+def find_body_hack_type(line,saved_tables):
+    '''
+    CSM [000B806D] on_body_part body_hack_type 4 len 10744, elem_hide_state 1, isChunk 1, isGzip false
+    '''
+    if not "body_hack_type" in line:
+        return
+
+    reg_str = r"CSM\s+\[(\S+)\].+body_hack_type (\w+)"
+    matchObj = re.search(reg_str, line)
+    if matchObj:
+        csm_id = matchObj.group(1)
+        hack_type = matchObj.group(2)
+        if "0x" in hack_type:
+            hack_type = int(hack_type,16)
+
+        saved_tables[csm_id] = int(hack_type)
+
+
+
 class QueryCmd():
     def get_port(self,logcat_path,csm_id):
         print "get_port,csm_id=" + str(csm_id)
@@ -54,18 +93,21 @@ class QueryCmd():
                 find_port_info_from_netlog(line,csm_id)
         pass
 
-    def list_host(self,logcat_path="logcat.log"):
+    def list_traffic(self,logcat_path="logcat.log"):
         '''
         CSM [0009801C] find host m.youtube.com
         '''
         hosts_table = {}
-
+        elmhide_url_table = {}
+        hack_type_table = {}
         with open(str(logcat_path), "r") as ins:
             for line in ins:
                 find_host(line,hosts_table)
-
+                find_elem_hide_url(line,elmhide_url_table)
+                find_body_hack_type(line,hack_type_table)
         for key,value in hosts_table.iteritems():
-            print "{} -> {}".format(key,value)
+            print "{} -> {}:{}".format(key,value,hack_type_table.get(key,0))
+
 
     def get_tcpdump(self):
         cmd = "rm -rf trace/ "

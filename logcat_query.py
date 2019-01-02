e82b1c2ef26b190c26685050ddbe8cf4b993426b
simon <lashwang@outlook.com>
2019-01-02 15:55:37 +0800

    add logcat query tools

--- logcat_query.py
+++ logcat_query.py
@@ -16,25 +16,56 @@ import glob
 port_query_reg_str = r"CSM\s+\[{}\].*client_src_port\s+(\d+),\s+csp_src_port\s+(\d+)"
 adclear_apk_path = "adclear/build/outputs/apk/adclearInternal/debug"
 
+
+
+
+def find_port_info_from_netlog(line,csm_id):
+    if "addNetLogRecord:" not in line:
+        return
+
+    splited = line.split(",")
+    csm_in_netlog = splited[-4]
+    if int(csm_id, 16) == int(csm_in_netlog, 16):
+        local_port = splited[-2]
+        remote_port = splited[-1]
+        if int(remote_port) == 0 or int(local_port) == 0:
+            print "find zero port in netlogs:" + line
+            return
+        print "query result for [{}]:tcp.port eq {} or tcp.port eq {}".format(csm_id, local_port, remote_port)
+        exit(0)
+
+
+def find_host(line,saved_tables):
+    if not "find host" in line:
+        return
+    reg_str = r"CSM\s+\[(\S+)\]\s+find host\s+(\S+)"
+    matchObj = re.search(reg_str, line)
+    if matchObj:
+        csm_id = matchObj.group(1)
+        host = matchObj.group(2)
+        saved_tables[csm_id] = host
+
+
 class QueryCmd():
     def get_port(self,logcat_path,csm_id):
-        print "get_port,csm_id=" + csm_id
+        print "get_port,csm_id=" + str(csm_id)
         with open(str(logcat_path), "r") as ins:
             for line in ins:
-                if csm_id not in line:
-                    continue
-                if "client_src_port" not in line and "csp_src_port" not in line:
-                    continue
-                #print line
-                reg_str = port_query_reg_str.format(csm_id)
-                matchObj = re.search(reg_str, line)
-                if matchObj:
-                    local_port = matchObj.group(1)
-                    remote_port = matchObj.group(2)
-                    print "query result for [{}]:tcp.port eq {} or tcp.port eq {}".format(csm_id,local_port,remote_port)
-                    return
-
+                find_port_info_from_netlog(line,csm_id)
         pass
+
+    def list_host(self,logcat_path="logcat.log"):
+        '''
+        CSM [0009801C] find host m.youtube.com
+        '''
+        hosts_table = {}
+
+        with open(str(logcat_path), "r") as ins:
+            for line in ins:
+                find_host(line,hosts_table)
+
+        for key,value in hosts_table.iteritems():
+            print "{} -> {}".format(key,value)
 
     def get_tcpdump(self):
         cmd = "rm -rf trace/ "

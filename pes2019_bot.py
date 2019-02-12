396a4d4457ee5b3181ee3354f16d9968760e7f32
simon <swang@seven.com>
2019-02-13 00:06:59 +0800

    update pes2019 bot script.

--- pes2019_bot.py
+++ pes2019_bot.py
@@ -9,21 +9,23 @@ from pywinauto.application import Application
 import win32gui
 import win32ui
 import ctypes
-
+import win32con
+import time
+import os
 
 MessageBox = ctypes.windll.user32.MessageBoxW
-EnumWindows = ctypes.windll.user32.EnumWindows
-EnumWindowsProc = ctypes.WINFUNCTYPE(ctypes.c_bool, ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int))
-GetWindowText = ctypes.windll.user32.GetWindowTextW
-GetWindowTextLength = ctypes.windll.user32.GetWindowTextLengthW
-IsWindowVisible = ctypes.windll.user32.IsWindowVisible
+# EnumWindows = ctypes.windll.user32.EnumWindows
+# EnumWindowsProc = ctypes.WINFUNCTYPE(ctypes.c_bool, ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int))
+# GetWindowText = ctypes.windll.user32.GetWindowTextW
+# GetWindowTextLength = ctypes.windll.user32.GetWindowTextLengthW
+# IsWindowVisible = ctypes.windll.user32.IsWindowVisible
 
 
 
 
 PS_REMOTE_WINDOW_TITLE = "PS4遥控操作"
 PS_MACRO_WINDOW_TITLE = "PS4 Macro"
-
+PS_IMG_SEARCH_PATH = "./pes2019_img_search"
 
 
 all_window_titles = []
@@ -70,6 +72,27 @@ def get_window_size(hwnd):
     return (x,y,w,h)
 
 
+def get_window_screenshot(hwnd):
+    (x,y,w,h) = get_window_size(hwnd)
+    wDC = win32gui.GetWindowDC(hwnd)
+    dcObj = win32ui.CreateDCFromHandle(wDC)
+    cDC = dcObj.CreateCompatibleDC()
+    dataBitMap = win32ui.CreateBitmap()
+    dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
+    cDC.SelectObject(dataBitMap)
+    cDC.BitBlt((0, 0), (w, h), dcObj, (0, 0), win32con.SRCCOPY)
+    dataBitMap.SaveBitmapFile(cDC, "test.bmp")
+    # Free Resources
+    dcObj.DeleteDC()
+    cDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, wDC)
+    win32gui.DeleteObject(dataBitMap.GetHandle())
+
+
+def press_ps4_as_forground():
+    pos = pyautogui.locateCenterOnScreen(os.path.join(PS_IMG_SEARCH_PATH,'ps_remote_logo.png'),confidence=0.9)
+    print(pos)
+
 class CmdInterface():
     def get_screen(self):
         im2 = pyautogui.screenshot('my_screenshot.png')
@@ -89,12 +112,34 @@ class CmdInterface():
     def list_ps4_windows(self):
         hld_ps4_remote,hld_ps4_macro = find_ps4_related_window()
         print(hld_ps4_remote,hld_ps4_macro)
-        win32gui.SetForegroundWindow(hld_ps4_remote)
+        win32gui.MoveWindow(hld_ps4_remote, 0, 0, 1920, 1080, True)
+
         (x,y,w,h) = get_window_size(hld_ps4_remote)
         print(w,h)
+        get_window_screenshot(hld_ps4_remote)
 
 
+    def waiting_ps4_as_foreground(self):
+        while True:
+            text = win32gui.GetWindowText(win32gui.GetForegroundWindow())
+            print("get focus window:" + text)
+            if PS_REMOTE_WINDOW_TITLE in text:
+                break
+            time.sleep(3)
 
+        pyautogui.screenshot('my_screenshot.png')
+
+    def pes2019_bot_loop(self):
+        while True:
+            text = win32gui.GetWindowText(win32gui.GetForegroundWindow())
+            print("get focus window:" + text)
+            if PS_REMOTE_WINDOW_TITLE not in text:
+                press_ps4_as_forground()
+                time.sleep(3)
+                continue
+            screen_now = pyautogui.screenshot()
+
+            time.sleep(3)
 
 def main():
     fire.Fire(CmdInterface)
